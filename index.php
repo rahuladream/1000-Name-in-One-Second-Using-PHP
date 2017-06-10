1b90af0198c3184fb881b0c1eb94a01292afb87e
Rahul Singh <rahul.singh4800@gmail.com>
2017-06-10 03:12:08 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+/***************************************
+* HINDI NAME GENERATOR
+* Version: 1.0
+* FILE: index.php
+* AUTHOR: Rahul Singh
+* DATE: 09.06.2017
+* DETAILS: GENERATE MILLIONS OF UNIQUE NAME IN ONE CLICK
+***************************************/
+?>
+<?php 
+require('config/class.config.php');
+?>
+<!doctype html>
+<html lang="en-US"><head>
+<title>Hindi Indian Name Generator </title>
+<h3> Hindi Indian Name Generator </h3>
+<a href="./boy"> Boy Name Generator </a>
+<br/>
+<a href="./girl"> Girl Name Generator </a>
+</head>
+</html>
